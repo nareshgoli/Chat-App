0b3f033de251054191a474dca350df0e10d5fea5
nareshgoli <venkatanaresh515@gmail.com>
2019-08-13 15:38:52 +0530

    Added Templates

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.contrib.auth.models import User
+User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_name', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_30_mesages(self):
+        return Message.objects.oredr_by('-timestamp').all()[:30]    
